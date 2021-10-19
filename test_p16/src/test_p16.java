ade9e0f66de3ff6dda012dbd247098c8438f8679
chijong <t108360121@ntut.org.tw>
2021-10-19 21:22:39 +0800

    origin

---
+++ test_p16/src/test_p16.java
@@ -0,0 +1,11 @@
+
+public class test_p16 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println('A');
+		System.out.println("Åwªï¨Ï¥ÎJava!");
+		System.out.println(123);
+	}
+
+}
